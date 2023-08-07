dd2eaa09844f66bf9809201a13d519450013fe44
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-08-07 15:10:30 +0100

    smth

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const UserRouter = require('./routes/UserRouter');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors()) // add your client domain in here like { origin: 'localhost:5172' }
 
 app.use('/auth', AuthRouter)
 app.use('/teams', validateToken, TeamRouter)
